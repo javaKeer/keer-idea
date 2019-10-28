214bb72e6fa0619ede5945f4316b4821b7316e14
keer <1500465781@qq.com>
2019-10-28 20:05:06 +0800

    首次测试....

---
+++ src/main/java/github.java
@@ -0,0 +1,8 @@
+public class github {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("/*这个是一个跟Github的练习*/");
+        }
+    }
+}
